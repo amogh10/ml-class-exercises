14ec82a9be98e6cc0e771fb3d163b3abf733ffca
Estanislau Trepat <estanis@etrepat.com>
2011-10-21 23:47:37 +0200

    Finished programming exercise 1.

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -5,17 +5,26 @@ function J = computeCostMulti(X, y, theta)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = size(X,2); % number of features
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+total_sum = 0;
+for i=1:m,
+  % feature data
+  x           = X(i, :)';
+  % h(0) = 0tx
+  hypothesis  = theta' * x;
+  cost_sum    = (hypothesis - y(i))^2;
+  total_sum   = total_sum + cost_sum;
+end
 
-
-
+J = total_sum / (2*m);
 
 % =========================================================================
 
--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -9,28 +9,43 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
+  % ====================== YOUR CODE HERE ======================
+  % Instructions: Perform a single gradient step on the parameter vector
+  %               theta.
+  %
+  % Hint: While debugging, it can be useful to print out the values
+  %       of the cost function (computeCostMulti) and gradient here.
+  %
 
+  % number of features
+  n = size(X, 2);
 
+  % copy vector theta
+  theta_tmp = theta;
 
+  % iterate over each theta
+  for j=1:n,
 
+    % compute: sum( h(x(i)) - y(i) ) for 1 <= i <= m
+    sum_part = 0;
+    for i=1:m,
+      x = X(i, :)';
+      h = theta' * x;
 
+      sum_part = sum_part + ((h - y(i)) * X(i,j));
+    end
 
+    % compute theta(j)
+    theta_tmp(j) = theta_tmp(j) - (alpha * (sum_part / m));
+  end
 
+  % copy over theta_tmp to final theta
+  theta = theta_tmp;
 
+  % ============================================================
 
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
+  % Save the cost J in every iteration
+  J_history(iter) = computeCostMulti(X, y, theta);
 
 end
 
