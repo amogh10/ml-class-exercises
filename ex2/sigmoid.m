66856e0f1563a8a980c8becd3514670991775c30
Estanislau Trepat <estanis@etrepat.com>
2011-10-30 20:41:44 +0100

    Add implementation of sigmoid(x) function calculation

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -2,16 +2,18 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
-
-
+for r=1:size(g,1),
+  for c=1:size(g,2),
+    g(r,c) = 1 / (1 + (e ^ (z(r,c) * -1)));
+  end
+end
 
 % =============================================================
 
