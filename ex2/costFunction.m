d91995226b7fc0857b03ebf768db9a1e9e4c3543
Estanislau Trepat <estanis@etrepat.com>
2011-10-30 20:42:14 +0100

    Implement cost function J computation

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -7,9 +7,9 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% You need to return the following variables correctly
+J     = 0;
+grad  = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -20,12 +20,31 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% Calculate J(theta)
 
+  cost_sum = 0;
+  for i=1:m,
+    x   = X(i, :)';
+    hx  = sigmoid(theta' * x);
 
+    cost_sum = cost_sum + (-1 * y(i) * log(hx)) - ((1 - y(i)) * log(1 - hx));
+  end
 
+  J = cost_sum / m;
 
+% Calculate gradients for each theta-i
 
+  k = size(theta, 1);
+  for j=1:k,
+    sum_tmp = 0;
+    for i=1:m,
+      x = X(i, :)';
+      h = sigmoid(theta' * x);
+      sum_tmp = sum_tmp + ((h - y(i)) * X(i,j));
+    end
 
+    grad(j) = sum_tmp / m;
+  end
 
 % =============================================================
 
