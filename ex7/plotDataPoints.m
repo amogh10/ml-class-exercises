a619a71868ffe8fba309c240c935bfaab6fe6ded
Estanislau Trepat <estanis@etrepat.com>
2011-12-04 21:15:17 +0100

    Make plotDataPoints work under OSX's Octave.

--- ex7/plotDataPoints.m
+++ ex7/plotDataPoints.m
@@ -1,7 +1,7 @@
 function plotDataPoints(X, idx, K)
 %PLOTDATAPOINTS plots data points in X, coloring them so that those with the same
 %index assignments in idx have the same color
-%   PLOTDATAPOINTS(X, idx, K) plots data points in X, coloring them so that those 
+%   PLOTDATAPOINTS(X, idx, K) plots data points in X, coloring them so that those
 %   with the same index assignments in idx have the same color
 
 % Create palette
@@ -9,6 +9,7 @@ palette = hsv(K + 1);
 colors = palette(idx, :);
 
 % Plot the data
-scatter(X(:,1), X(:,2), 15, colors);
+colormap(jet);
+scatter(X(:,1), X(:,2), 15, idx);
 
 end
